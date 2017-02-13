95218243057046c388850478993a97de936e50d0
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-02-13 11:37:54 +0300

    add ngn-env folder option

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var exec = require('gulp-exec');
 
 module.exports = function(opt) {
-  var cmd = 'php ./ngn-env/run/run.php "new CliAccessArgsSingle(\'html <%= file.path %> ' + //
+  var cmd = 'php ' + //
+    (opt.ngnEnvFolder || './ngn-env') + //
+    '/run/run.php "new CliAccessArgsSingle(\'html <%= file.path %> ' + //
     opt.buildFolder + ' ' + opt.name + '\', new JsBuildTool)" ngn-cs';
   return exec(cmd, {
     continueOnError: false,
-      pipeStdout: true
+    pipeStdout: true
   });
 };
 module.exports.reporter = exec.reporter;
